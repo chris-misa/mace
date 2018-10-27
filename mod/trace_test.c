fa0586aabe9c4f04b5c98f754e9ee36cd941bb4e
Chris Misa <cmisa@uoregon.edu>
2018-10-27 16:01:50 -0700

    Moving to be able to listen to multiplie tracepoints

--- mod/trace_test.c
+++ mod/trace_test.c
@@ -14,9 +14,9 @@ MODULE_DESCRIPTION("Probing tracepoints to monitor network stack latency");
 static struct tracepoint *probe_tracepoint = NULL;
 
 void
-probe_func(void *unused, struct sk_buff *skb, int rc, struct net_device *dev, unsigned int len)
+probe_net_dev_xmit(void *unused, struct sk_buff *skb, int rc, struct net_device *dev, unsigned int len)
 {
-	printk("In probe function, skb: %x, dev: %x, len: %d\n", skb, dev, len);
+	printk("In net_dev_xmit probe, skb: %x, dev: %x, len: %d\n", skb, dev, len);
   if (dev) {
     if (dev->name) {
       printk("dev->name: %s\n", dev->name);
@@ -25,20 +25,32 @@ probe_func(void *unused, struct sk_buff *skb, int rc, struct net_device *dev, un
   }
 }
 
+void
+probe_sys_enter_sendto(void *unused)
+{
+  printk("In sento probe\n");
+}
+
 
 void
 test_and_set_tracepoint(struct tracepoint *tp, void *priv)
 {
+  int ret = 0;
+  int found = 0;
 	if (!strcmp(tp->name, "net_dev_xmit")) {
-		if (tracepoint_probe_register(tp, probe_func, NULL) != 0) {
-			printk(KERN_ALERT "Failed to register for trace event.\n");
-			probe_tracepoint = NULL;
-			printk(KERN_ALERT "Failed to set tracepoint.\n");
-		} else {
-			probe_tracepoint = tp;
-			printk("Set trace probe\n");
-		}
-	}
+		ret = tracepoint_probe_register(tp, probe_net_dev_xmit, NULL);
+    found = 1;
+  }
+  if (found) {
+    if (ret != 0) {
+      printk(KERN_ALERT "Failed to register for trace event.\n");
+      probe_tracepoint = NULL;
+      printk(KERN_ALERT "Failed to set tracepoint.\n");
+    } else {
+      probe_tracepoint = tp;
+      printk("Set trace probe\n");
+    }
+  }
 }
 
 int __init
@@ -53,7 +65,7 @@ void __exit
 trace_test_exit(void)
 {
 	if (probe_tracepoint) {
-		if (tracepoint_probe_unregister(probe_tracepoint, probe_func, NULL) != 0) {
+		if (tracepoint_probe_unregister(probe_tracepoint, probe_net_dev_xmit, NULL) != 0) {
 			printk(KERN_ALERT "Failed to unregister trace probe.\n");
 		}
 	}
