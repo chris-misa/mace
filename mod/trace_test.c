68a9b8be091e42a854d53fa79aec352f451a59c2
Chris Misa <cmisa@uoregon.edu>
2018-11-11 11:28:45 -0800

    finished receive side

--- mod/trace_test.c
+++ mod/trace_test.c
@@ -1,4 +1,14 @@
+//
+// Kernel module to measure network stack's send and receive latency
+//
+// Currently targets ping measurements.
+//
+// 2018, Chris Misa
+// 
+
 #define LINUX
+
+#define LOG_EVENTS
 
 #include <linux/module.h>
 #include <linux/kernel.h>
@@ -19,16 +29,33 @@
 #define MSG_SIZE 256
 #define BUF_SIZE 256
 
+#define SYSCALL_SENDTO 44
+#define SYSCALL_RECVMSG 47
+
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Chris Misa <cmisa@cs.uoregon.edu>");
 MODULE_DESCRIPTION("Probing tracepoints to monitor network stack latency");
 
-static struct tracepoint *probe_tracepoint[2];
-static unsigned long long send_time;
-static unsigned long long *ring_buffer;
+static struct tracepoint *probe_tracepoint[3];
+
+static int expect_send = 0;
+static int expect_recv = 0;
+static int ping_on_wire = 0;
+
+static long long send_time = 0;
+static long long recv_time = 0;
+
+struct rtt_lats {
+  unsigned long long send;
+  unsigned long long recv;
+};
+
+static struct rtt_lats *ring_buffer;
 static unsigned long head;
 static unsigned long tail;
+
+static struct rtt_lats cur_lat;
 
 DECLARE_WAIT_QUEUE_HEAD(wait_q);
 
@@ -57,29 +84,62 @@ probe_net_dev_xmit(void *unused, struct sk_buff *skb, int rc, struct net_device 
 {
   unsigned long long cur_time;
   unsigned long long delta_time;
-  if (dev) {
+  if (expect_send && dev) {
     cur_time = rdtsc();
     delta_time = cur_time - send_time;
-    printk("[%llu] dev->ifindex: %d\n", cur_time, dev->ifindex);
-    printk("delta time: %llu\n", delta_time);
+#ifdef LOG_EVENTS
+    printk("[%llu] net_dev_xmit with dev->ifindex: %d, delta time: %llu\n", cur_time, dev->ifindex, delta_time);
+#endif
+    expect_send = 0;
+    ping_on_wire = 1;
     
-    // Push onto queue
-    ring_buffer[head] = delta_time;
-    head = (head + 1) % BUF_SIZE;
-    printk("pushed onto queue at %lu\n", head);
-
-    // Wake up any waiting readers
-    wake_up(&wait_q);
+    cur_lat.send = delta_time;
   }
+}
+
+void
+probe_netif_receive_skb(void *unused, struct sk_buff *skb)
+{
+  recv_time = rdtsc();
+#ifdef LOG_EVENTS
+  printk("[%llu] netif_receive_skb with dev->ifindex: %d\n", recv_time, skb->dev->ifindex);
+#endif
+  expect_recv = 1;
 }
 
 void
 probe_sys_enter(void *unused, struct pt_regs *regs, long id)
 {
-  // Hard-coded sendto syscall number
-  if (id == 44) {
-    send_time = rdtsc();
-    printk("[%llu] probably a sendto\n", send_time);
+  unsigned long long cur_time;
+  unsigned long long delta_time;
+  switch (id) {
+    case SYSCALL_SENDTO:
+      send_time = rdtsc();
+#ifdef LOG_EVENTS
+      printk("[%llu] sendto\n", send_time);
+#endif
+      expect_send = 1;
+      break;
+    case SYSCALL_RECVMSG:
+      if (expect_recv && ping_on_wire) {
+        cur_time = rdtsc();
+        delta_time = cur_time - recv_time;
+#ifdef LOG_EVENTS
+        printk("[%llu] recv delta time: %llu\n", cur_time, delta_time);
+#endif
+        expect_recv = 0;
+        ping_on_wire = 0;
+
+        cur_lat.recv = delta_time;
+
+        // Push onto queue
+        ring_buffer[head] = cur_lat;
+        head = (head + 1) % BUF_SIZE;
+
+        // Wake up any waiting readers
+        wake_up(&wait_q);
+      }
+      break;
   }
 }
 
@@ -98,6 +158,10 @@ test_and_set_tracepoint(struct tracepoint *tp, void *priv)
 		ret = tracepoint_probe_register(tp, probe_sys_enter, NULL);
     probe_tracepoint[1] = tp;
     found = 1;
+  } else if (!strcmp(tp->name, "netif_receive_skb")) {
+    ret = tracepoint_probe_register(tp, probe_netif_receive_skb, NULL);
+    probe_tracepoint[2] = tp;
+    found = 1;
   }
   
   if (found) {
@@ -114,9 +178,10 @@ trace_test_init(void)
 {
   probe_tracepoint[0] = NULL;
   probe_tracepoint[1] = NULL;
+  probe_tracepoint[2] = NULL;
 
   // Get memory for buffers
-  ring_buffer = kmalloc(sizeof(long long int) * BUF_SIZE, GFP_KERNEL);
+  ring_buffer = kmalloc(sizeof(struct rtt_lats) * BUF_SIZE, GFP_KERNEL);
   if (ring_buffer == NULL) {
     printk(KERN_ALERT "Failed to alloc memory.\n");
     return -ENOMEM;
@@ -139,6 +204,7 @@ trace_test_init(void)
     return Major;
   }
   printk("mace_test assigned major number %d\n", Major);
+  printk("use \"mknod <path to new device file> c %d 0\"\n", Major);
 
 	// Find the tracepoint and set probe function
 	for_each_kernel_tracepoint(test_and_set_tracepoint, NULL);
@@ -151,14 +217,20 @@ trace_test_exit(void)
 {
 	if (probe_tracepoint[0]) {
 		if (tracepoint_probe_unregister(probe_tracepoint[0], probe_net_dev_xmit, NULL) != 0) {
-			printk(KERN_ALERT "Failed to unregister trace probe.\n");
+			printk(KERN_ALERT "Failed to unregister net_dev_xmit traceprobe.\n");
 		}
 	}
 	if (probe_tracepoint[1]) {
 		if (tracepoint_probe_unregister(probe_tracepoint[1], probe_sys_enter, NULL) != 0) {
-			printk(KERN_ALERT "Failed to unregister trace probe.\n");
+			printk(KERN_ALERT "Failed to unregister sys_enter traceprobe.\n");
 		}
 	}
+  if (probe_tracepoint[2]) {
+    if (tracepoint_probe_unregister(probe_tracepoint[2], probe_netif_receive_skb, NULL) != 0) {
+      printk(KERN_ALERT "Failed to unregister netif_receive_skb traceprobe.\n");
+    }
+  }
+
 	printk("Removed trace probes.\n");
   if (ring_buffer) {
     kfree(ring_buffer);
@@ -221,7 +293,9 @@ device_read(struct file *fp, char *buf, size_t len, loff_t *offset)
     }
 
     // Form the string, dequeue
-    sprintf(msg, "delta_time: %llu\n", ring_buffer[tail]);
+    sprintf(msg, "send latency: %llu recv latency: %llu\n",
+      ring_buffer[tail].send,
+      ring_buffer[tail].recv);
     msgp = msg;
     tail = (tail + 1) % BUF_SIZE;
   }
