b68555445c741e5a8f1752e6d26f8a226787598f
Chris Misa <cmisa@uoregon.edu>
2018-12-07 22:04:17 -0800

    Removed arch-dependent file

--- test_trace/.tmp_versions/trace_test.mod
+++
@@ -1,3 +0,0 @@
-/home/mace/mod/trace_test.ko
-/home/mace/mod/trace_test.o
-
--- test_trace/.trace_test.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/mace/mod/trace_test.ko := ld -r -m elf_x86_64  -z max-page-size=0x200000 -T ./scripts/module-common.lds  --build-id  -o /home/mace/mod/trace_test.ko /home/mace/mod/trace_test.o /home/mace/mod/trace_test.mod.o ;  true
