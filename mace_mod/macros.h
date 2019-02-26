38ea15fd5d75e89ace864c7d4d1adfb76f42cc72
Chris Misa <chris@chrismisa.com>
2019-02-26 01:45:16 +0000

    Initial simple static flat-bucket implementation

--- mace_mod/macros.h
+++ mace_mod/macros.h
@@ -12,32 +12,19 @@
 /*
  * Register entry of packet into latency segment.
  *
- *   table: name of static table of struct mace_latency
- *   index: index into above table
- *   hash_table: the kernel hash table object
- *   key_ptr: a pointer to the hash key
+ *   table: name of static table
+ *   key: key generated from packet data
  *
- * Note: might need to synchronize instruction re-ordering before valid = 1
  */
-#define register_entry(table, index, hash_table, key_ptr) \
+#define register_entry(table, key) \
 { \
-  struct mace_latency *ml; \
-  unsigned long long ts = rdtsc(); \
-  unsigned long flags; \
-  ml = table + atomic_inc_return(&index); \
-  if (atomic_read(&index) == MACE_LATENCY_TABLE_SIZE) { \
-    atomic_set(&index, 0); \
-  } \
-  if (ml->valid) { \
-    hash_del(&ml->hash_list); \
-  } \
+  int index = hash_min((key), MACE_LATENCY_TABLE_BITS); \
+  long unsigned flags; \
   \
-  spin_lock_irqsave(&ml->lock, flags); \
-  ml->enter = ts; \
-  ml->key = *(key_ptr); \
-  ml->valid = 1; \
-  hash_add(hash_table, &ml->hash_list, ml->key); \
-  spin_unlock_irqrestore(&ml->lock, flags); \
+  spin_lock_irqsave(&table[index].lock, flags); \
+  table[index].enter = rdtsc(); \
+  table[index].key = (key); \
+  spin_unlock_irqrestore(&table[index].lock, flags); \
 }
 
 /*
@@ -47,22 +34,19 @@
  *   key: hash key
  *   direction: mace_latency_type to hand to mace_push_event()
  */
-#define register_exit(hash_table, key, direction) \
+#define register_exit(table, key, direction) \
 { \
-  struct mace_latency *ml; \
-  unsigned long long dt; \
+  int index = hash_min((key), MACE_LATENCY_TABLE_BITS); \
+  unsigned long long dt = 0; \
   unsigned long flags; \
-  hash_for_each_possible(hash_table, ml, hash_list, key) { \
-    if (ml && ml->key == (key)) { \
-      /* if this entry is locked, it must be in the process of being overwritten */ \
-      if (spin_trylock_irqsave(&ml->lock, flags)) {\
-        dt = rdtsc() - ml->enter; \
-        ml->valid = 0; \
-        hash_del(&ml->hash_list); \
-        spin_unlock_irqrestore(&ml->lock, flags); \
-        mace_push_event(dt, direction); \
-      } \
-      break; \
-    } \
+  \
+  spin_lock_irqsave(&table[index].lock, flags); \
+  if (table[index].key == (key)) { \
+    dt = rdtsc() - table[index].enter; \
+  } \
+  spin_unlock_irqrestore(&table[index].lock, flags); \
+  \
+  if (dt != 0) { \
+    mace_push_event(dt, direction); \
   } \
 }
