8d46460b34c93c8023bc8bb81afe2e3040d76c2f
Chris Misa <cmisa@cs.uoregon.edu>
2019-05-24 23:15:16 -0600

    Added characterization experiment scripts

---
+++ tests/characterization/report_size_stats.r
@@ -0,0 +1,181 @@
+#
+# R script to parse ping and latency data from run_params.sh test run
+#
+source("readPingFile.r")
+
+#
+# Argument should be directory holding raw data
+#
+args <- commandArgs(trailingOnly=T)
+if (length(args) != 1) {
+  stop("Need path to data")
+}
+
+options(digits=22)
+
+data_path <- args[1]
+SAVED_DATA_PATH <- paste(data_path, "/saved_r_data", sep="")
+SUMMARY_DATA_PATH <- paste(data_path, "/summary", sep="")
+
+#
+# Wrapers around utility parsing functions sourced above
+#
+read_ping <- function(line) {
+  d <- readPingFile(paste(data_path, "/", line, sep=""))
+  cat("Read ", length(d$rtt), " rtts from ", line, "\n")
+  d
+}
+
+#
+# Begin main work
+#
+
+cat("---------------Running Report for run_params.sh------------\n")
+
+#
+# Load data, either saved from previous run or from raw files
+#
+if (file.exists(SAVED_DATA_PATH)) {
+  cat("  Found saved data\n")
+  rtts <- dget(SAVED_DATA_PATH)
+
+} else {
+  cat("  Reading raw data\n")
+
+  rtts <- list(
+    native = list(means = c(), sds = c(), sizes = c(), raw = list()),
+    container = list(means = c(), sds = c(), sizes = c(), raw = list())
+  )
+
+  con <- file(paste(data_path, "/manifest", sep=""), "r")
+  while (T) {
+    line <- readLines(con, n=1)
+    if (length(line) == 0) {
+      break
+    }
+    read <- 0
+
+    linePattern <- ".*_([0-9\\.]+)\\.ping"
+    matches <- grep(linePattern, line, value=T)
+
+    # Branch for ping files
+    if (length(matches) != 0) {
+
+      size <- as.numeric(sub(linePattern, "\\1", matches))
+      
+      # Branch for native ping files
+      if (length(grep("native", line)) != 0) {
+
+        newRtts <- read_ping(line)
+        rtts$native$means <- c(rtts$native$means, mean(newRtts$rtt))
+        rtts$native$sds <- c(rtts$native$sds, sd(newRtts$rtt))
+        rtts$native$sizes <- c(rtts$native$sizes, size)
+        rtts$native$raw[[length(rtts$native$raw) + 1]] <- newRtts$rtt
+        read <- 1
+      }
+
+      # Branch for container files
+      if (length(grep("container", line)) != 0) {
+
+        newRtts <- read_ping(line)
+        rtts$container$means <- c(rtts$container$means, mean(newRtts$rtt))
+        rtts$container$sds <- c(rtts$container$sds, sd(newRtts$rtt))
+        rtts$container$sizes <- c(rtts$container$sizes, size)
+        rtts$container$raw[[length(rtts$container$raw) + 1]] <- newRtts$rtt
+        read <- 1
+      }
+    }
+
+    # Print per-file message
+    if (read == 0) {
+      cat("Warning: skipping unknown file:", line, "\n")
+    }
+  }
+  close(con)
+
+  if (!identical(rtts$container$sizes, rtts$native$sizes)) {
+    cat("Native and contaienr runs have different sizes!\n")
+  }
+
+  dput(rtts, file=SAVED_DATA_PATH)
+}
+
+#
+# Plot means
+#
+meanDiffs <- rtts$container$means - rtts$native$means
+
+f <- lm(Y~X, data=data.frame(X=rtts$native$sizes, Y=meanDiffs))
+s <- summary(f)
+capture.output(s, file=paste(data_path, "/summary.txt", sep=""))
+
+pdf(file=paste(data_path, "/mean_diffs.pdf", sep=""))
+par(mar=c(4,4,2,2))
+# add 8 to payload size to get packet size
+#plot(rtts$native$sizes + 8, meanDiffs, pch=19, col=rgb(0,0,0,0.5),
+#    xlab="Packet Size (bytes)",
+#    ylab=expression(paste("RTT Mean Difference (",mu,"s)", sep="")),
+#    main="")
+boxplot(Y~X, data=data.frame(X=rtts$native$sizes + 8, Y=meanDiffs),
+    xlab="Packet Size (bytes)",
+    ylab=expression(paste("RTT Mean Difference (",mu,"s)", sep="")),
+    main="")
+#abline(f)
+dev.off()
+
+
+
+#
+# Plot raw mean values (not as differences)
+#
+pdf(file=paste(data_path, "/raw_means.pdf", sep=""))
+par(mar=c(4,4,2,2), cex=0.85)
+plot(0, type="n", xlim=range(rtts$native$sizes),
+                  ylim=range(rtts$native$means, rtts$container$means))
+lines(rtts$native$sizes, rtts$native$means, type="p", pch=1)
+lines(rtts$container$sizes, rtts$container$means, type="p", pch=2)
+
+dev.off()
+
+#
+# Plot raw data values (not as means)
+#
+colors <- rep(c(rgb(0,0,0,0.2), rgb(0,0,0,0.2)), length(rtts$native$raw))
+
+nativeRawFrame <- data.frame(rtt=c(), size=c())
+for (i in 1:length(rtts$native$sizes)) {
+  nativeRawFrame <- rbind(nativeRawFrame,
+    data.frame(rtt=rtts$native$raw[[i]],
+               size=rep(rtts$native$sizes[[i]], length(rtts$native$raw[[i]]))))
+}
+
+containerRawFrame <- data.frame(rtt=c(), size=c())
+for (i in 1:length(rtts$container$sizes)) {
+  containerRawFrame <- rbind(containerRawFrame,
+    data.frame(rtt=rtts$container$raw[[i]],
+               size=rep(rtts$container$sizes[[i]], length(rtts$container$raw[[i]]))))
+}
+
+pdf(file=paste(data_path, "/raw_data_native.pdf", sep=""))
+par(mar=c(4,4,2,2), cex=0.8)
+stripchart(rtt~size, data=nativeRawFrame,
+        vertical=T, pch=20, col=colors, ylim=c(0,100))
+dev.off()
+
+pdf(file=paste(data_path, "/raw_data_container.pdf", sep=""))
+par(mar=c(4,4,2,2), cex=0.8)
+stripchart(rtt~size, data=containerRawFrame,
+        vertical=T, pch=20, col=colors, ylim=c(0,100))
+dev.off()
+
+containerRawFrame$size <- containerRawFrame$size + 4
+
+# Absolute hack to get both these on same page. . ..
+pdf(file=paste(data_path, "/raw_data.pdf", sep=""))
+par(mar=c(4,4,2,2), cex=0.8)
+stripchart(rtt~size, data=rbind(nativeRawFrame, containerRawFrame),
+        vertical=T, pch=20, col=colors, ylim=c(0,100))
+dev.off()
+
+
+cat("Done.\n")
---
+++ tests/characterization/run_size_stats.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+#
+# Measure the effect of ping parameters on RTT difference between container and native contexts
+#
+
+export B="===================="
+
+export NUM_ROUNDS=2
+
+export PING_ARGS="-D -i 0.0 -c 2000"
+
+export SIZES="`seq 16 100 1472`"
+
+export TARGET="10.10.1.2"
+
+export MACE_PATH=`echo ${PWD%${PWD##*/}}`
+
+#export NATIVE_PING_CMD="${MACE_PATH}/iputils/ping"
+
+export NATIVE_PING_CMD="ping"
+export CONTAINER_PING_CMD="/iputils/ping"
+
+export PING_CONTAINER_IMAGE="chrismisa/slow-ping"
+export PING_CONTAINER_NAME="ping-container"
+
+export META_DATA="metadata"
+export MANIFEST="manifest"
+
+export PAUSE_CMD="sleep 2"
+
+export DATE_STR=`date +%Y%m%d%H%M%S`
+
+export ANALYSIS_CMD="Rscript report_params.r"
+
+mkdir $DATE_STR
+pushd $DATE_STR > /dev/null
+
+echo -e "$B uname -a $B\n $(uname -a)\n" >> $META_DATA
+echo -e "$B lshw: $B\n $(lshw)\n" >> $META_DATA
+
+echo $B Running params test $B
+
+#
+# Spin up container
+#
+docker run -itd --name=$PING_CONTAINER_NAME \
+	  --entrypoint=/bin/bash \
+	  --cpuset-cpus=0-0 \
+	  $PING_CONTAINER_IMAGE \
+	  > /dev/null
+echo "  Ping container up"
+
+for R in `seq 1 $NUM_ROUNDS`
+do
+  echo $B Round $R $B
+  for S in $SIZES
+  do
+
+    echo "  Running with size: $S"
+
+    #
+    # Native Control userspace base-line
+    #
+    taskset 0x1 $NATIVE_PING_CMD $PING_ARGS -s $S $TARGET >> ${R}native_${S}.ping
+    echo "    Took native control"
+    echo "${R}native_${S}.ping" >> $MANIFEST
+    $PAUSE_CMD
+    
+    #
+    # Container Control base-line
+    #
+    docker exec $PING_CONTAINER_NAME \
+      $CONTAINER_PING_CMD $PING_ARGS -s $S $TARGET >> ${R}container_${S}.ping
+    echo "    Took container control"
+    echo "${R}container_${S}.ping" >> $MANIFEST
+    $PAUSE_CMD
+
+  done
+done
+
+popd > /dev/null
+
+echo "  Generating graphs and analysis"
+$ANALYSIS_CMD $DATE_STR
+
+docker stop $PING_CONTAINER_NAME > /dev/null
+docker rm $PING_CONTAINER_NAME > /dev/null
+
+echo $DATE_STR is done.
